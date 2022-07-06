ad3e67f99f22f378d99d80eeb9f71ff0429babb5
TorokJani <torokjanos@gmail.com>
2022-07-06 22:17:24 +0200

    init

---
+++ src/main/java/com/example/supersprinter2000/Supersprinter2000Application.java
@@ -0,0 +1,13 @@
+package com.example.supersprinter2000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Supersprinter2000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Supersprinter2000Application.class, args);
+	}
+
+}
